7983137e2b967d6b6f8720dbda4b65a32bd40fe7
Vincent Howard <vincenthoward@Vincents-MacBook-Pro.local>
2017-10-30 16:32:37 -0700

    Server set up, auth0 set up without SQL files and Installed dependencies. Server can be launched with just 'nodemon'

---
+++ server/server.js
@@ -0,0 +1,83 @@
+const express = require('express'),
+        bodyParser = require('body-parser'),
+        massive = require('massive'),
+        session = require('express-session'),
+        passport = require('passport'),
+        cors = require('cors'),
+        Auth0Strategy = require('passport-auth0');
+
+require ('dotenv').config();
+// =============================================================================
+// EXPRESS AND EXPRESS SESSIONS
+// =============================================================================
+const app = express();
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+// =============================================================================
+// CORS CONTROLLERS
+// =============================================================================
+app.use(cors());
+// =============================================================================
+// PASSPORT AND BODYPARSERs
+// =============================================================================
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
+// app.use(express.static(`${__dirname}./../build`));
+// =============================================================================
+// MASSIVE - CONNECTION STRING FOR DATABASE
+// =============================================================================
+massive(process.env.CONNECTION_STRING).then( db => {
+    app.set('db', db);
+    const PORT = 3333;
+    app.listen(PORT, () => console.log('Listening on port: ', PORT))
+})
+// =============================================================================
+// PASSPORT STRATEGY
+// =============================================================================
+passport.use( new Auth0Strategy({
+domain: process.env.AUTH_DOMAIN,
+clientID: process.env.AUTH_CLIENT_ID,
+clientSecret: process.env.AUTH_CLIENT_SECRET,
+callbackURL: process.env.AUTH_CALLBACK
+}, function(accessToken, refreshToken, extraParams, profile, done) {
+const db = app.get('db');
+        db.get_user([profile.identities[0].user_id]).then( user => {
+        if (user[0]) {
+                done(null, user[0].id)
+        } else {
+                db.create_user([
+                profile.emails[0].value,
+                profile.identities[0].user_id]).then( user => {
+                        done(null, user[0].id)
+ })
+}})
+}))
+passport.serializeUser(function(userId, done) {
+        done(null, userId);
+})
+        passport.deserializeUser( function( userId, done) {
+        app.get('db').current_user(userId).then(user => {
+                done(null, user[0])
+})
+})
+// =============================================================================
+// AUTH ENDPOINTS
+// =============================================================================
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0',{
+        successRedirect: `${process.env.SERVERHOST}/#/`,
+        failureRedirect: '/auth'
+}))
+app.get('/auth/logout', (req,res) => {
+        req.logOut();
+        res.redirect(302, `https:${process.env.AUTH_DOMAIN}/v2/logout?returnTo=${process.env.SERVERHOST}`)
+})
+app.get('/api/user',  passport.authenticate('auth0'), (req, res) => {
+        req.app.get('db').current_user().then(user =>{
+        res.status(200).send(user)
+        }).catch((err) => {console.log(err)})
+})
