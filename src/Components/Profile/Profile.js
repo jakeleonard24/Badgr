458259181a00b072a9ca02d5c68d22e1ece368dc
Andrew-Cunningham <andrewjasoncunningham@yahoo.com>
2017-11-01 15:15:35 -0700

    user profile usnername and img

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import {getCurrentUser} from './../../ducks/reducer';
+import axios from 'axios';
 
 class Profile extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            value:'',
+            currentUser:[]}
+    }
+    componentDidMount(){
+        axios.get('/api/user').then((response)=>{
+            console.log('your user info is: ',response.data); // ex.: { user: 'Your User'}
+            console.log(response.status); // ex.: 200
+            this.setState({
+                currentUser:response.data
+            })
+          });
+    }
     render() {
+
         return (
             <div>
+                <div>
                 Profile Page
-
                 <Link to='/home'>
                 <button>Home</button>
                 </Link>
+                </div>    
+                <div>
+                   Welcome, {this.state.currentUser.username}!
+                </div>
+                <div>
+                    <img src={this.state.currentUser.picture}/>
+                </div>
             </div>
         );
     }
